9db76d71ca3d7c0dc9c849144a1cd5c9f5478e4f
rovilay <rovilayjnr01@gmail.com>
2023-08-24 10:27:12 +0100

    - Continuous delivery Vercel

--- api/test_app.py
+++ api/test_app.py
@@ -1,6 +1,5 @@
 import json
-from app import app
-from utils import constants
+from app import app, DUMMY_BOOKS
 
 def test_get_all_books():
     response = app.test_client().get('/api/v1/books')
@@ -8,8 +7,8 @@ def test_get_all_books():
 
     assert response.status_code == 200
     assert type(books) is list
-    assert len(books) is len(constants.DUMMY_BOOKS)
+    assert len(books) is len(DUMMY_BOOKS)
     assert type(books[0]) is dict
     assert type(books[1]) is dict
-    assert books[0]['title'] == constants.DUMMY_BOOKS[0]['title']
-    assert books[1]['title'] == constants.DUMMY_BOOKS[1]['title']
+    assert books[0]['title'] == DUMMY_BOOKS[0]['title']
+    assert books[1]['title'] == DUMMY_BOOKS[1]['title']
